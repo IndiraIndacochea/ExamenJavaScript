82da32fd5dc7077a1a9ec392a7b6501f2fabdd55
Indira <dira.indacocheag@gmail.com>
2022-08-25 21:25:32 -0500

    ejercicio condiciones

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ExamenJavaScript
 primer examen FullStack JavaScript
+let horasOrdinarias = 265;
+let horasExtraordinarias = 350;
---
+++ condiciones.js
@@ -0,0 +1,19 @@
+let hora1=40;
+let hora2= 53-hora1;
+let horasOrdinarias = hora1*265;
+let horasExtraordinarias = hora2*350;
+
+if (hora1<=40) {
+    console.log( "El total a pagar por horas ordinarias es: " + horasOrdinarias);
+}
+else {
+
+    console.log( "El total a pagar por horas Extraordinarias es: " + horasExtraordinarias);
+}{
+  console.log( "El total a pagar por horas Extraordinarias es: " + horasExtraordinarias);
+ 
+}
+
+
+
+ 
